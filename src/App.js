52bc41cc0e8c4fb8a48c53bfef57d05d2aa109a3
Tuukka Virtanen <duukkis@gmail.com>
2023-01-11 17:27:41 +0200

    simple landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <h1>Kuinka aamutorkku olet?</h1>
+          <h2>Tee tämä testi niin tiedät!</h2>
+          <Button variant="primary">Tee testi</Button>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
