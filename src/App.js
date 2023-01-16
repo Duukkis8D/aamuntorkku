fcab11acbb0e77b61508208971ec4089fd4d3f1b
Tuukka Virtanen <duukkis@gmail.com>
2023-01-16 10:40:00 +0200

    run prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Container>
-        <Row>
-          <h1>Kuinka aamutorkku olet?</h1>
-          <h2>Tee tämä testi niin tiedät!</h2>
-          <Button variant="primary">Tee testi</Button>
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Row>
+                    <h1>Kuinka aamuntorkku olet?</h1>
+                    <h2>Tee tämä testi niin tiedät!</h2>
+                    <Button variant="primary">Tee testi</Button>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
