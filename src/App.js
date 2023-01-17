5cdda014c267d23040b8aff2dbaa21d3b08006b5
Tuukka Virtanen <duukkis@gmail.com>
2023-01-17 13:34:25 +0200

    vertically align landing page content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
 
 function App() {
     return (
         <div className="App">
             <Container>
-                <Row>
-                    <h1>Kuinka aamuntorkku olet?</h1>
-                    <h2>Tee tämä testi niin tiedät!</h2>
-                    <Button variant="primary">Tee testi</Button>
+                <Row className="d-flex align-items-center viewPortHeight100">
+                    <Col>
+                        <h1>Kuinka aamuntorkku olet?</h1>
+                        <p>Tee tämä testi niin tiedät!</p>
+                        <Button variant="primary">Tee testi</Button>
+                    </Col>
                 </Row>
             </Container>
         </div>
