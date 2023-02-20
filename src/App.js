7d22c14ca8cab7186571f31f68be81d15c368ecc
Tuukka Virtanen <duukkis@gmail.com>
2023-02-20 17:15:01 +0200

    add outer container for styling purposes

--- src/App.js
+++ src/App.js
@@ -36,22 +36,24 @@ export default function App() {
 
     return (
         <div className="App">
-            <Container className="p-2 border border-primary border-3 rounded viewport-height-100">
-                <Form>
-                    {currentStep === 0 && <LandingPage nextPhase={nextPhase} />}
-                    {currentStep > 0 && (
-                        <Test
-                            currentStep={currentStep}
-                            totalSteps={totalSteps}
-                            nextPhase={nextPhase}
-                            previousPhase={previousPhase}
-                            startOver={startOver}
-                            formData={formData}
-                            handleChange={handleChange}
-                            handleChangeReactSelect={handleChangeReactSelect}
-                        />
-                    )}
-                </Form>
+            <Container className="p-2 min-vh-100">
+                <Container className="p-3 border border-primary border-3 rounded">
+                    <Form>
+                        {currentStep === 0 && <LandingPage nextPhase={nextPhase} />}
+                        {currentStep > 0 && (
+                            <Test
+                                currentStep={currentStep}
+                                totalSteps={totalSteps}
+                                nextPhase={nextPhase}
+                                previousPhase={previousPhase}
+                                startOver={startOver}
+                                formData={formData}
+                                handleChange={handleChange}
+                                handleChangeReactSelect={handleChangeReactSelect}
+                            />
+                        )}
+                    </Form>
+                </Container>
             </Container>
         </div>
     );
