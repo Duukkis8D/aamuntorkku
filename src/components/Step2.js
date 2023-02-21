2aab665684c5110cb1e2ec87ed2901bf957e2f91
Tuukka Virtanen <duukkis@gmail.com>
2023-02-21 15:17:52 +0200

    add step 2 radio buttons to React state

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -2,7 +2,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Form } from 'react-bootstrap';
 
-export default function Step2() {
+export default function Step2({ formData, handleChange }) {
     return (
         <Row id="step-2-page" className="h-50">
             <Col>
@@ -16,12 +16,18 @@ export default function Step2() {
                         name="step2RadioButton"
                         id="step2RadioButtonYes"
                         label="kyllä"
+                        value="kyllä"
+                        onChange={(event) => handleChange('step2RadioButton', event.target.value)}
+                        checked={formData.step2RadioButton === 'kyllä'}
                     />
                     <Form.Check
                         type="radio"
                         name="step2RadioButton"
                         id="step2RadioButtonNo"
                         label="ei"
+                        value="ei"
+                        onChange={(event) => handleChange('step2RadioButton', event.target.value)}
+                        checked={formData.step2RadioButton === 'ei'}
                     />
                 </Form.Group>
             </Col>
